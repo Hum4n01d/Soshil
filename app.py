c4d07cfb938e83578030913cdcd1aaa42b382b2b
Hum4n01d <hum4n01d@icloud.com>
2016-07-04 17:28:45 -0700

    Added bcrypt init to import

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import requests
 
 from flask import Flask, g, render_template, flash, redirect, url_for, request, abort
-from flask_bcrypt import check_password_hash
+from flask_bcrypt import Bcrypt, check_password_hash
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_gravatar import Gravatar
 
