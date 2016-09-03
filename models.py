82b4798c0cda2ec0d4899ce23e3df1a5ff40fa2e
Hum4n01d <hum4n01d@icloud.com>
2016-09-02 18:12:54 -0700

    longer posts

--- models.py
+++ models.py
@@ -84,7 +84,7 @@ class User(UserMixin, BaseModel):
 
 class Post(BaseModel):
     title = CharField(max_length=100)
-    content = CharField(max_length=250)
+    content = CharField(max_length=500)
     raw_content = CharField(default='')
 
     timestamp = DateTimeField(default=datetime.now)
