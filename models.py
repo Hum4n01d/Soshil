3ae0780b423097d9cef3ead1abb287a0a441cfaa
Hum4n01d <hum4n01d@icloud.com>
2016-09-02 18:14:42 -0700

    fixed charfield for post to textfield

--- models.py
+++ models.py
@@ -84,7 +84,7 @@ class User(UserMixin, BaseModel):
 
 class Post(BaseModel):
     title = CharField(max_length=100)
-    content = CharField(max_length=500)
+    content = TextField()
     raw_content = CharField(default='')
 
     timestamp = DateTimeField(default=datetime.now)
