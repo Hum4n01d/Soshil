be1f60e0258f698cbb5f6a8d59712cb25170b895
Hum4n01d <hum4n01d@icloud.com>
2016-09-10 09:18:40 -0700

    fixed github icon code lol

--- fixicons.py
+++ fixicons.py
@@ -1,8 +1,8 @@
 import hashlib
 import models
 
-models.User.update(models.User.avatar_url == 'https://avatars2.githubusercontent.com/u/8015809?v=3&s=75').where(models.User.username ** "brld").execute()
+models.User.update(avatar_url='https://avatars2.githubusercontent.com/u/8015809?v=3&s=75').where(models.User.username ** "brld").execute()
 
-models.User.update(models.User.avatar_url == 'https://avatars3.githubusercontent.com/u/17019573?v=3&s=75').where(models.User.username ** "ianardo").execute()
+models.User.update(avatar_url='https://avatars3.githubusercontent.com/u/17019573?v=3&s=75').where(models.User.username ** "ianardo").execute()
 
-models.User.update(models.User.avatar_url == 'https://avatars3.githubusercontent.com/u/17228477?v=3&s=75').where(models.User.username ** "hum4n01d").execute()
+models.User.update(avatar_url='https://avatars3.githubusercontent.com/u/17228477?v=3&s=75').where(models.User.username ** "hum4n01d").execute()
