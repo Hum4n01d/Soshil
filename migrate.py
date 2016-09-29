4348190a38f14930df94b0dfc1a46428c70699cd
Hum4n01d <hum4n01d@icloud.com>
2016-09-29 14:18:02 -0700

    fix migrategsh

--- migrate.py
+++ migrate.py
@@ -4,7 +4,6 @@ import models
 
 migrator = PostgresqlMigrator(models.db_proxy)
 
-old_raw_content = CharField(default='')
 new_raw_content = TextField(default='')
 
 def do_migration():
@@ -12,5 +11,5 @@ def do_migration():
         migrator.add_column('post', 'raw_content', new_raw_content)
     )
     
-    for post in posts:
+    for post in Post.select():
         models.Post.update(raw_content=post.new_raw_content).execute()
