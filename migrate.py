d35050017e8f6ebd2e78b1e04d973f3377f89b3b
Hum4n01d <hum4n01d@icloud.com>
2016-09-29 14:15:20 -0700

    migration part 2

--- migrate.py
+++ migrate.py
@@ -8,15 +8,6 @@ old_raw_content = CharField(default='')
 new_raw_content = TextField(default='')
 
 def do_migration():
-    posts = models.Post.select()
-        
-    for post in posts:
-        models.Post.update(new_raw_content=post.raw_content).execute()
-        
-    migrate(
-        migrator.drop_column('post', 'raw_content', old_raw_content)
-    )
-        
     migrate(
         migrator.add_column('post', 'raw_content', new_raw_content)
     )
