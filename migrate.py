b8a3ac6bf5a423e3e422c0ec79c0e547730298b4
Hum4n01d <hum4n01d@icloud.com>
2016-09-29 14:39:58 -0700

    fixed field

--- migrate.py
+++ migrate.py
@@ -7,9 +7,5 @@ migrator = PostgresqlMigrator(models.db_proxy)
 new_raw_content = TextField(default='')
 
 def do_migration():
-    migrate(
-        migrator.add_column('post', 'raw_content', new_raw_content)
-    )
-    
     for post in Post.select():
         models.Post.update(raw_content=post.new_raw_content).execute()
